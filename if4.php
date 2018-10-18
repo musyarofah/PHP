b1605e210c771b2d62e3e666f88ee232f8a5d202
musyarofah <43978950+musyarofah@users.noreply.github.com>
2018-10-18 09:18:04 +0700

    Add files via upload

---
+++ if4.php
@@ -0,0 +1,16 @@
+<?php
+if($ucha=="A"){
+echo "Nilai = "Sangat Baik";
+}elseif($ucha=="B")
+{
+echo "Nilai = Baik";
+}elseif($ucha=="C"){
+echo"Nilai = Cukup";
+}elseif($ucha==D"){
+echo"Nilai = kurang";
+}elseif($ucha=="E"){
+echo"Nilai=kebangetan";
+}else{
+Echo"masukan nilai A-E";
+}
+?>
